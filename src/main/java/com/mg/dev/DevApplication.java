4dc73d461bfa08c51531b4b5fcd6fe635b9eb201
Jaewoo Kim <wodndhksk@gmail.com>
2024-04-05 21:38:21 +0900

    refactor: Aspect 빈 등록 방식 변경

--- src/main/java/com/mg/dev/DevApplication.java
+++ src/main/java/com/mg/dev/DevApplication.java
@@ -1,17 +1,14 @@
 package com.mg.dev;
 
-import com.mg.dev.postgres.aop.advice.LogTraceAspect;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@Import(LogTraceAspect.class)
 public class DevApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/mg/dev/postgres/config/AspectConfig.java
@@ -0,0 +1,13 @@
+package com.mg.dev.postgres.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy // Aspect 자동 프록시 활성화
+public class AspectConfig {
+//    @Bean
+//    public LogTraceAspect logTraceAspect() {
+//        return new LogTraceAspect();
+//    }
+}
