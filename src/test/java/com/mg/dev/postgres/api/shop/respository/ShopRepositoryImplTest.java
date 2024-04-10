94d07f9c097e10e399be65e536ed2af37c4b2b62
Jaewoo Kim <wodndhksk@gmail.com>
2024-04-10 18:04:55 +0900

    test: null 방어 로직 테스트

---
+++ src/test/java/com/mg/dev/postgres/api/shop/respository/ShopRepositoryImplTest.java
@@ -0,0 +1,45 @@
+package com.mg.dev.postgres.api.shop.respository;
+
+import com.mg.dev.postgres.api.shop.dto.ShopReqDto;
+import com.querydsl.core.BooleanBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class ShopRepositoryImplTest {
+
+    @Autowired
+    ShopRepositoryImpl shopRepository;
+
+    /**
+     * dto 가 null 일 경우 동작 테스트
+     */
+    @Test
+    void excepitonTest() {
+//        ShopReqDto shopReqDto = ShopReqDto.builder()
+//                .name("")
+//                .addr("")
+//                .number("")
+//                .city("")
+//                .build();
+
+        // dot 가 null 일 경우
+        ShopReqDto shopReqDto = ShopReqDto.builder().build();
+        log.info("ShopReqDto log={}", shopReqDto);
+
+        try {
+            shopRepository.shopEntityList(shopReqDto);
+        }catch (IllegalArgumentException e){
+            Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> shopRepository.shopEntityList(shopReqDto));
+        }
+
+    }
+
+}
